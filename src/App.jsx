7458728de799f8c44ddbe5ba937af91620a29346
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2024-01-18 14:50:41 +0530

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
+
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
 const App = () => {
   return (
-    <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">
-        hero
-      </section>
-      <section className="padding">
-        popular product
-      </section>
-      <section className="padding">
-        Super quality
-      </section>
-      <section className="padding-x padding-y">
-        services
-      </section>
-      <section className="padding">
-        special offer
-      </section>
-      <section className="bg-pale-blue padding">
-        customer review
-      </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
-        subscribe
-      </section>
-      <section className="bg-black padding-x padding-t pb-8">
-        footer
-      </section>
+    <main className='relative'>
       
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffers />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SpecialOffers from './SpecialOffers';
+import SuperQuality from './SuperQuality'
+
+
+export{
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    Subscribe,
+    SpecialOffers,
+    SuperQuality
+}
