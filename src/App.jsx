f03b2e21417590e03eceea2e92c9ea29853613cc
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2024-01-18 14:17:59 +0530

    building app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        hero
+      </section>
+      <section className="padding">
+        popular product
+      </section>
+      <section className="padding">
+        Super quality
+      </section>
+      <section className="padding-x padding-y">
+        services
+      </section>
+      <section className="padding">
+        special offer
+      </section>
+      <section className="bg-pale-blue padding">
+        customer review
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        subscribe
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        footer
+      </section>
+      
+    </main>
   )
 }
+
+export default App
